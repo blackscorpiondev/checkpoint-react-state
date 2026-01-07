50303cbe92b9256e3efba88a33cc6d9c36aea35c
blackscorpiondev <black.scorpion.dev@gmail.com>
2026-01-07 14:21:40 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+//class stat extends React.component 
+class App extends Component {
+   constructor() {
+     super();
+     this.state = {
+       Personne: {
+         fullName: 'black scorpion',
+         bio: 'je suis calme mais dangereux.',
+         imgSrc: 'https://www.shutterstock.com/image-vector/scorpion-tattoo-design-vector-artwork-260nw-2469456375.jpg',
+         profession: 'developpeur'
+       },
+      montre: false,
+      tempsEcoule: 0
+    };
+    this.timer = null;
+  }
 
-export default App;
+  basculerAffichage = () => {
+     this.setState(prevState => ({ montre: !prevState.montre })); // this.setstate({montre: !this.state.montre})
+   };
+ 
+   componentDidMount() {
+     this.timer = setInterval(() => {
+       this.setState(prevState => ({
+         tempsEcoule: prevState.tempsEcoule + 1
+       }));
+     }, 1000); // Mise à jour chaque seconde
+   }
+ 
+   componentWillUnmount() {
+     clearInterval(this.timer);
+   }
+ 
+   render() {
+     return (
+       <div className="app-container">
+         <div className="header">
+          <h1 className="title">Mon Profil React</h1>
+        </div>
+         <button onClick={this.basculerAffichage}>
+           {this.state.montre ? 'Cacher le profil' : 'Afficher le profil'}
+         </button>
+         {this.state.montre && (
+          <div className="profil">
+             <img src={this.state.Personne.imgSrc} alt="profil" />
+             <h2>{this.state.Personne.fullName}</h2>
+             <h4>{this.state.Personne.profession}</h4>
+             <p>{this.state.Personne.bio}</p>
+         </div>
+         )}
+
+        <hr />
+         <p>Temps écoulé depuis le montage : <strong>{this.state.tempsEcoule} secondes</strong></p>
+       </div>
+     );
+   }
+ }
+
+ export default App;
